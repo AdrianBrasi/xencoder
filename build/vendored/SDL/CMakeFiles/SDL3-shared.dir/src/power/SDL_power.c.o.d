44e6ce5342b18a74022ebdcd965c6ac6e4997417
Adrian Brasi <brasi@mailfence.com>
2025-09-02 19:29:22 -0700

    Stop tracking build directory

--- build/vendored/SDL/CMakeFiles/SDL3-shared.dir/src/power/SDL_power.c.o.d
+++
@@ -1,5 +0,0 @@
-vendored/SDL/CMakeFiles/SDL3-shared.dir/src/power/SDL_power.c.o: \
- /home/brasi/projects/xencoder/vendored/SDL/src/power/SDL_power.c \
- /usr/include/stdc-predef.h \
- /home/brasi/projects/xencoder/vendored/SDL/src/SDL_internal.h \
- /home/brasi/projects/xencoder/vendored/SDL/src/power/SDL_syspower.h
--- build/vendored/SDL/CMakeFiles/SDL3-shared.dir/src/render/psp/SDL_render_psp.c.o.d
+++
@@ -1,4 +0,0 @@
-vendored/SDL/CMakeFiles/SDL3-shared.dir/src/render/psp/SDL_render_psp.c.o: \
- /home/brasi/projects/xencoder/vendored/SDL/src/render/psp/SDL_render_psp.c \
- /usr/include/stdc-predef.h \
- /home/brasi/projects/xencoder/vendored/SDL/src/SDL_internal.h
